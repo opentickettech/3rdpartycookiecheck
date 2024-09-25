123885f75392b6f0aae2f521788f571edfc6b184
Tom Groot <tom@eventix.nl>
2024-09-25 13:30:32 +0200

    chore: remove script and added .gitignore

--- sync.sh
+++
@@ -1,2 +0,0 @@
-aws s3 cp start.html s3://mindmup-3rdptycheck --cache-control="max-age=8640000" --acl public-read
-aws s3 cp complete.html s3://mindmup-3rdptycheck --cache-control="max-age=8640000" --acl public-read
